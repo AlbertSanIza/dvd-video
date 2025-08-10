23861c9378fbdf4c134f790f6b27de9b66828f6f
Albert Sanchez <albert.san.iza@gmail.com>
2025-08-10 12:46:27 -0700

    Add canvas resize handling to DVDVideo class
    Introduces width and height properties and a resize method to update the canvas size based on its bounding client rect. Adds a window resize event listener to ensure the canvas resizes dynamically.

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,24 @@ import './style.css'
 
 class DVDVideo {
     canvas: HTMLCanvasElement = document.getElementById('dvd-video-canvas') as HTMLCanvasElement
+    width = 0
+    height = 0
 
     constructor() {
         this.init()
+        this.setupEventListeners()
+    }
+
+    resize() {
+        const { width, height } = this.canvas.getBoundingClientRect()
+        this.canvas.width = width
+        this.canvas.height = height
+    }
+
+    setupEventListeners() {
+        window.addEventListener('resize', () => this.resize())
+    }
+
     }
 
     init() {}
