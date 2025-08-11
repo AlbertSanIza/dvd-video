bbc2ab124fb44c7dd6d7a6fd50a71637dbb12f96
Albert Sanchez <albert.san.iza@gmail.com>
2025-08-10 18:08:57 -0700

    Add Rectangle class with properties and methods for position, dimensions, and speed

---
+++ src/rectangle.ts
@@ -0,0 +1,23 @@
+import { Point } from './point'
+
+export class Rectangle {
+    position: Point
+    width: number
+    height: number
+    private _speed: number
+
+    constructor(position: Point, width: number, height: number, speed: number) {
+        this.position = position
+        this.width = width
+        this.height = height
+        this._speed = speed
+    }
+
+    get speed() {
+        return this._speed
+    }
+
+    get center() {
+        return this.position.y + this.height / 2
+    }
+}
